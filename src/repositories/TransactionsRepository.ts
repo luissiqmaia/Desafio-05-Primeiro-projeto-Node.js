53812ffa8f823483aa85a558a5155a13bbf550ee
Luis Henrique <luismaya78@gmail.com>
2020-11-06 18:42:27 -0300

    Desafio conluído!

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable class-methods-use-this */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionBalanceModel {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +20,34 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionBalanceModel {
+    return {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (accumulatorT: Balance, nextT: Transaction) => {
+        if (nextT.type === 'income') {
+          accumulatorT.income += nextT.value;
+          accumulatorT.total += nextT.value;
+        } else {
+          accumulatorT.outcome += nextT.value;
+          accumulatorT.total -= nextT.value;
+        }
+        return accumulatorT;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(transaction: Transaction): Transaction {
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
@@ -8,8 +9,32 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ type, title, value }: Omit<Transaction, 'id'>): Transaction {
+    if (!/income|outcome/.test(type)) {
+      throw new Error('Invalid type of transition.');
+    }
+
+    if (type === 'income' && value < 0) {
+      throw new Error('For this type of transaction you must income.');
+    }
+
+    if (type === 'outcome' && value < 0) {
+      throw new Error('For this type of transaction you must outcome.');
+    }
+
+    if (
+      type === 'outcome' &&
+      value > this.transactionsRepository.getBalance().total
+    ) {
+      throw new Error(
+        'The value of income do not be greater than income value available!',
+      );
+    }
+
+    const transition = new Transaction({ type, title, value });
+
+    this.transactionsRepository.create(transition);
+    return transition;
   }
 }
 
